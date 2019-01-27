25ac322a127db20eec20ce6b222af267b74729f8
unknown <2992625868@qq.com>
2019-01-27 17:38:52 +0800

    1.27 城市 商铺列表

--- elm/src/router/router.js
+++ elm/src/router/router.js
@@ -1,7 +1,9 @@
 import App from '../App'
 
 const home = r => require.ensure([], () => r(require('../page/home/home')), 'home')
+const city = r => require.ensure([], () => r(require('../page/city/city')), 'city')
 const login = r => require.ensure([], () => r(require('../page/login/login')), 'login')
+const msite = r => require.ensure([], () => r(require('../page/msite/msite')), 'msite')
 
 export default [{
     path: '/',
@@ -17,9 +19,21 @@ export default [{
             path: '/home',
             component: home
         }, 
+        //登录注册页
         {
             path: '/login',
             component: login
+        },
+          //当前选择城市页
+          {
+            path: '/city/:cityid',
+            component: city
+        },
+         //所有商铺列表页
+         {
+            path: '/msite',
+            component: msite,
+            meta: { keepAlive: true },
         },
     ]
 }]
