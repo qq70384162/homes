0f5417bdb9744782fbf9630b5d4fb7c93397a38b
unknown <2992625868@qq.com>
2019-01-30 19:46:44 +0800

    1.30

--- elm/src/router/router.js
+++ elm/src/router/router.js
@@ -13,7 +13,25 @@ const add = r => require.ensure([], () => r(require('../page/profile/children/ch
 const addDetail = r => require.ensure([], () => r(require('../page/profile/children/children/children/children/addDetail')), 'addDetail')
 const service = r => require.ensure([], () => r(require('../page/service/service')), 'service')
 const questionDetail = r => require.ensure([], () => r(require('../page/service/children/questionDetail')), 'questionDetail')
-
+const confirmOrder = r => require.ensure([], () => r(require('../page/confirmOrder/confirmOrder')), 'confirmOrder')
+const remark = r => require.ensure([], () => r(require('../page/confirmOrder/children/remark')), 'remark')
+const payment = r => require.ensure([], () => r(require('../page/confirmOrder/children/payment')), 'payment')
+const userValidation = r => require.ensure([], () => r(require('../page/confirmOrder/children/userValidation')), 'userValidation')
+const invoice = r => require.ensure([], () => r(require('../page/confirmOrder/children/invoice')), 'invoice')
+const chooseAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/chooseAddress')), 'chooseAddress')
+const addAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/children/addAddress')), 'addAddress')
+const searchAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/children/children/searchAddress')), 'searchAddress')
+const find = r => require.ensure([], () => r(require('../page/find/find')), 'find')
+const download = r => require.ensure([], () => r(require('../page/download/download')), 'download')
+const food = r => require.ensure([], () => r(require('../page/food/food')), 'food')
+const points = r => require.ensure([], () => r(require('../page/points/points')), 'points')
+// const order = r => require.ensure([], () => r(require('../page/order/order')), 'order')
+// const orderDetail = r => require.ensure([], () => r(require('../page/order/children/orderDetail')), 'orderDetail')
+const benefit = r => require.ensure([], () => r(require('../page/benefit/benefit')), 'benefit')
+const forget = r => require.ensure([], () => r(require('../page/forget/forget')), 'forget')
+const balance = r => require.ensure([], () => r(require('../page/balance/balance')), 'balance')
+const balanceDetail = r => require.ensure([], () => r(require('../page/balance/children/detail')), 'balanceDetail')
+const pointsDetail = r => require.ensure([], () => r(require('../page/points/children/detail')), 'pointsDetail')
 export default [{
     path: '/',
     component: App, //顶层路由，对应index.html
@@ -76,6 +94,86 @@ export default [{
                 path: 'service', //服务中心
                 component: service,
             },]
+        },
+          //确认订单页
+          {
+            path: '/confirmOrder',
+            component: confirmOrder,
+            children: [{
+                path: 'remark', //订单备注
+                component: remark,
+            }, {
+                path: 'invoice', //发票抬头
+                component: invoice,
+            }, {
+                path: 'payment', //付款页面
+                component: payment,
+            }, {
+                path: 'userValidation', //用户验证
+                component: userValidation,
+            }, {
+                path: 'chooseAddress', //选择地址
+                component: chooseAddress,
+                children: [{
+                    path: 'addAddress', //添加地址
+                    component: addAddress,
+                    children: [{
+                        path: 'searchAddress', //搜索地址
+                        component: searchAddress,
+                    }]
+                }, ]
+            }, ]
+        },
+         //修改密码页
+         {
+            path: '/forget',
+            component: forget
+        },
+         //订单列表页
+        //  {
+        //     path: '/order',
+        //     component: order,
+        //     children: [{
+        //         path: 'orderDetail', //订单详情页
+        //         component: orderDetail,
+        //     }, ]
+        // },
+          //发现页
+          {
+            path: '/find',
+            component: find
+        },
+        //下载页
+        {
+            path: '/download',
+            component: download
+        },
+         //服务中心
+         {
+            path: '/service',
+            component: service,
+             children: [{
+                path: 'questionDetail', //订单详情页
+                component: questionDetail,
+            }, ]
+        },
+        //余额
+        {
+            path: 'balance',
+            component: balance,
+            children: [{
+                path: 'detail', //余额说明
+                component: balanceDetail,
+            }, ]
+        },
+         //我的积分页
+         {
+            path: 'points',
+            component: points,
+            children: [{
+                path: 'detail', //积分说明
+                component: pointsDetail,
+            }, ]
         },
     ]
 }]
